1abb51e9558f2229c1151941011c556db42789fa
mrobin <mrobin@companeo.com>
2015-04-14 17:56:25 +0200

    comments + SynchronizingNode, closes #2

--- index.js
+++ index.js
@@ -1,61 +1,102 @@
+/**
+ * Workflow is a main part of js-workflow. You need to use it as starter point.
+ */
 class Workflow {
     constructor(data) {
         this.data = data;
-        this.nodes = [];
+        this.outNodes = [];
     }
 
+    /**
+     * Not like a lot of workflow frameworks in various languages, you can start a lot of things from the starting point.
+     * js-workflow allows you to add many node directly at the start and not after a first useless node.
+     * Corresponding to a split into parallel execution
+     */
     addOutNode(node) {
-        this.nodes.push(node);
+        this.outNodes.push(node);
     }
 
+    /**
+     * Classical toString method
+     */
     toString() {
         return 'data :' + JSON.stringify(this.data);
     }
 
+    /**
+     * Will run. Surprise ;)
+     */
     run() {
-        for (let n in this.nodes) {
-            this.nodes[n].run(this.data);
+        for (let n in this.outNodes) {
+            this.outNodes[n].run(this.data);
         }
     }
 }
 
+/**
+ * Node is the other main thing. Define the master pieces of a node behavior. You should not use it directly but inherit of it.
+ */
 class Node {
     constructor() {
-        this.nodes = [];
+        this.outNodes = [];
         this.finished = false;
     }
 
+    /**
+     * You can add a node here to follow this one. If you want, you can add more than one.
+     * Corresponding to a split into parallel execution.
+     */
     addOutNode(node) {
-        this.nodes.push(node);
+        this.outNodes.push(node);
     }
 
+    /**
+     * Is this node is finished ? Sometimes is useful. Mainly times to allows future nodes to execute.
+     */
     isFinished() {
         return this.finished;
     }
 
+    /**
+     * Set as finished
+     */
     setFinished() {
         this.finished = true;
     }
 
+    /**
+     * Can I run ? For me, it's always true. That's why you should not use me but an inherited class that controls my state correctly.
+     */
     canRun() {
         return true;
     }
 
+    /**
+     * Will run. Surprise ;)
+     * Again.
+     */
     run(data) {
         this.data = data;
-        for (let n in this.nodes) {
-            if (!this.nodes[n].isFinished() && this.nodes[n].canRun(data)) {
-                this.nodes[n].run(this.data);
+        for (let n in this.outNodes) {
+            if (!this.outNodes[n].isFinished() && this.outNodes[n].canRun(data)) {
+                this.outNodes[n].run(this.data);
             }
         }
     }
 }
+/**
+ * ConsoleNode is useful for debug. It only displays in console the data.
+ */
 class ConsoleNode extends Node {
     constructor(message) {
         super();
         this.message = message;
     }
 
+    /**
+     * Will run. Surprise ;)
+     * Again. Again.
+     */
     run(data) {
         this.data = data;
         if (undefined !== this.message) {
@@ -66,23 +107,19 @@ class ConsoleNode extends Node {
         super.run(this.data);
     }
 }
-class Add1Node extends Node {
-    constructor() {
-        super();
-    }
-
-    run(data) {
-        this.data = +data + 1;
-        super.setFinished();
-        super.run(this.data);
-    }
-}
+/**
+ * ConditionnalNode can run only if the condition passed in params is true.
+ * Takes an expression or a function, condition needs to be evaluated to boolean true or false only (strict equality, "true" will correspond to false).
+ */
 class ConditionnalNode extends Node {
     constructor(condition) {
         super();
         this.condition = condition;
     }
 
+    /**
+     * is the condition allowed me to run ?
+     */
     canRun(data) {
         var retour;
 
@@ -95,8 +132,82 @@ class ConditionnalNode extends Node {
         return retour;
     }
 
+    /**
+     * Read the super.run comment
+     */
     run(data) {
         this.data = data;
+        super.setFinished();
+        super.run(this.data);
+    }
+}
+/**
+ * SynchronizingNode is the reprenstation of logical operator AND
+ */
+class SynchronizingNode extends Node {
+    constructor() {
+        super();
+        this.inNodes = [];
+    }
+
+    addInNodes(node) {
+        this.inNodes.push(node);
+    }
+
+    canRun() {
+        var retour = true,
+            iterateurInNodes = 0,
+            inNodesLength = this.inNodes.length;
+
+        while (true === retour && iterateurInNodes < inNodesLength) {
+            if (!this.inNodes[iterateurInNodes].isFinished()) {
+                retour = false;
+            }
+            iterateurInNodes += 1;
+        }
+
+        return retour;
+    }
+}
+/**
+ * SynchronizingNode is the reprenstation of logical operator AND
+ */
+class SynchronizingNode extends Node {
+    constructor() {
+        super();
+        this.inNodes = [];
+    }
+
+    addInNodes(node) {
+        this.inNodes.push(node);
+    }
+
+    canRun() {
+        var retour = true,
+            iterateurInNodes = 0,
+            inNodesLength = this.inNodes.length;
+
+        while (true === retour && iterateurInNodes < inNodesLength) {
+            if (!this.inNodes[iterateurInNodes].isFinished()) {
+                retour = false;
+            }
+            iterateurInNodes += 1;
+        }
+
+        return retour;
+    }
+}
+
+/**
+ * Add1Node is a sample. It just
+ */
+class Add1Node extends Node {
+    constructor() {
+        super();
+    }
+
+    run(data) {
+        this.data = +data + 1;
         super.setFinished();
         super.run(this.data);
     }
