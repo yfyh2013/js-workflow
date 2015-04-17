9989b7f2a0345788d10157f124b8ea2c3b05af09
mrobin <mrobin@companeo.com>
2015-04-17 11:12:38 +0200

    more unit tests on Workflow class

--- tests/workflow.js
+++ tests/workflow.js
@@ -1,10 +1,62 @@
 /*globals require, describe, it */
 
 var Workflow = require('./../lib/workflow').Workflow;
+require('jasmine-expect');
 
 describe('Workflow', function () {
     it('should be defined', function (done) {
         expect(Workflow).toBeDefined();
+        done();
+    });
+
+    it('should have right data whith params', function (done) {
+        var wf = new Workflow();
+
+        expect(wf.data).not.toBeDefined();
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow(2);
+
+        expect(wf.data).toBe(2);
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow(0);
+
+        expect(wf.data).toBe(0);
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow(true);
+
+        expect(wf.data).toBe(true);
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow(false);
+
+        expect(wf.data).toBe(false);
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow('a');
+
+        expect(wf.data).toBe('a');
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow(new Date());
+
+        expect(wf.data).toBeDate();
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow({});
+
+        expect(wf.data).toBeEmptyObject();
+        expect(wf.outNodes).toBeEmptyArray();
+
+        wf = new Workflow({
+            a: 0
+        });
+
+        expect(wf.data).toBeNonEmptyObject();
+        expect(wf.outNodes).toBeEmptyArray();
+
         done();
     });
 
