9f5b9511c48d0f3b988d6ea11e1d65207b6e1888
mrobin <mrobin@companeo.com>
2015-05-19 19:04:13 +0200

    more unit tests

--- tests/driver.js
+++ tests/driver.js
@@ -8,6 +8,23 @@ describe('Driver', function () {
         done();
     });
 
+    it('should throw when bad call', function (done) {
+        try {
+            var node = Driver();
+            expect(2).toBe(3);
+        } catch (eX) {
+            expect(typeof eX).toBe('object');
+            expect(eX instanceof TypeError).toBeTruthy();
+        }
+        done();
+    });
+
+    it('should have right data', function (done) {
+        var theConsole = new Driver();
+        expect(theConsole).toBeEmptyObject();
+        done();
+    });
+
     describe('#store', function () {
         it('should be defined', function (done) {
             var driver = new Driver();
