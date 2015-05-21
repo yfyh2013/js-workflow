978a66c4ceec1ad56c4ccbe19c90ceb2ca4e2df9
mrobin <mrobin@companeo.com>
2015-05-21 12:19:35 +0200

    add unit tests to xorMergeNode

--- tests/xorMergeNode.js
+++ tests/xorMergeNode.js
@@ -48,6 +48,107 @@ describe('XorMergeNode', function () {
             done();
         });
 
+        it('should take the correct value when defining failCallback with good value', function (done) {
+            var condNode = new XorMergeNode(function () {
+            });
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            done();
+        });
+
+        it('should take the correct value when defining failCallback with dirty value', function (done) {
+            var condNode = new XorMergeNode(undefined);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(null);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(true);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(false);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(0);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(1);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(4);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(Infinity);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(-0);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(-1);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(-4);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode(-Infinity);
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            condNode = new XorMergeNode('abc');
+            expect(condNode.failCallback).toBeDefined();
+            expect(condNode.failCallback).toBeFunction();
+            done();
+        });
+
+        describe('#inNodes', function (done) {
+            it('#with a correct Node arg', function (done) {
+                var condNode = new XorMergeNode(),
+                    aNode = new Node();
+
+                try {
+                    condNode.addInNodes(aNode);
+                    expect(condNode.inNodes).toBeNonEmptyArray();
+                    expect(condNode.inNodes.length).toBe(1);
+                    expect(condNode.inNodes[0]).toBeObject();
+                    expect(condNode.inNodes[0] instanceof Node).toBeTruthy()
+                } catch (eX) {
+                    expect(2).toBe(3);
+                }
+                done();
+            });
+
+            it('#with a dirty arg', function (done) {
+                var condNode = new XorMergeNode(),
+                    aNode = new Node();
+
+                try {
+                    condNode.addInNodes();
+                    expect(true).toBeFalsy();
+                } catch (eX) {
+                    expect(3).toBe(3);
+                }
+                try {
+                    condNode.addInNodes(true);
+                    expect(true).toBeFalsy();
+                } catch (eX) {
+                    expect(3).toBe(3);
+                }
+                try {
+                    condNode.addInNodes(false);
+                    expect(true).toBeFalsy();
+                } catch (eX) {
+                    expect(3).toBe(3);
+                }
+                try {
+                    condNode.addInNodes(function () {
+                    });
+                    expect(true).toBeFalsy();
+                } catch (eX) {
+                    expect(3).toBe(3);
+                }
+                done();
+            });
+        });
+
         describe('#outNodes', function () {
             it('should be defined', function (done) {
                 var condNode = new XorMergeNode();
@@ -56,5 +157,35 @@ describe('XorMergeNode', function () {
                 done();
             });
         });
+
+        describe('#canRun', function () {
+            it('should be defined', function (done) {
+                var condNode = new XorMergeNode();
+                expect(condNode.canRun).toBeFunction();
+                done();
+            });
+
+            it('will return false for an empty inNode', function (done) {
+                var condNode = new XorMergeNode();
+                expect(condNode.canRun()).toBeDefined();
+                expect(condNode.canRun()).toBeBoolean();
+                expect(condNode.canRun()).toBeFalsy();
+                done();
+            });
+
+            it('will return false for a inNode which throw during canRun', function (done) {
+                var condNode = new XorMergeNode(),
+                    aFailNode = new Node();
+
+                aFailNode.isFinished = function () {
+                    throw '';
+                };
+                condNode.addInNodes(aFailNode);
+                expect(condNode.canRun()).toBeDefined();
+                expect(condNode.canRun()).toBeBoolean();
+                expect(condNode.canRun()).toBeFalsy();
+                done();
+            });
+        })
     });
 });
